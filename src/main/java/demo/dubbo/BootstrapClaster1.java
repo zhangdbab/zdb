260f146da0475eaad455d104430d2b4bd7febd53
张东彬(DJ009828) <dj009828zhangdongbin@djbx.com>
2020-11-02 15:50:50 +0800

    dubbo  负载均衡

---
+++ src/main/java/demo/dubbo/BootstrapClaster1.java
@@ -0,0 +1,19 @@
+package demo.dubbo;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import java.io.IOException;
+
+/**
+ * Created by DJ009828 on 2020/10/31 18:56
+ */
+public class BootstrapClaster1 {
+    public static void main(String[] args) throws IOException {
+        ClassPathXmlApplicationContext classPathXmlApplicationContext
+                =new ClassPathXmlApplicationContext("dubbo-cluster1.xml") ;
+        classPathXmlApplicationContext.start();
+        System.in.read();
+
+    }
+
+}
--- src/main/java/demo/dubbo/server/impl/HelloServiceImpl2.java
+++ src/main/java/demo/dubbo/server/impl/HelloServiceImpl2.java
@@ -5,9 +5,9 @@ import demo.dubbo.api.IHelloService;
 /**
  * Created by DJ009828 on 2020/10/31 18:35
  */
-public class HelloServiceImpl implements IHelloService {
+public class HelloServiceImpl2 implements IHelloService {
     @Override
     public String helle(String msg) {
-        return  "1,hello, "+msg;
+        return  "2,hello, "+msg;
     }
 }
