a59074053ef5aa66d2a9927853efd6e197b220f7
张东彬(DJ009828) <dj009828zhangdongbin@djbx.com>
2020-11-03 14:12:16 +0800

    Class getConstructor 方法测试

--- src/main/java/demo/dubbo/demo/User.java
+++ src/main/java/demo/dubbo/demo/User.java
@@ -1,9 +1,17 @@
 package demo.dubbo.demo;
 
 import com.alibaba.dubbo.common.extension.Activate;
+import com.alibaba.dubbo.rpc.Protocol;
 
 @Activate
 public class User {
+
+    private Protocol protocol ;
+
+    public User(Protocol protocol) {
+        this.protocol = protocol;
+    }
+
     @Activate
     public  void  say(){
 
--- src/main/java/demo/dubbo/demo/test.java
+++ src/main/java/demo/dubbo/demo/test.java
@@ -1,11 +1,15 @@
 package demo.dubbo.demo;
 
 import com.alibaba.dubbo.common.extension.Activate;
+import com.alibaba.dubbo.rpc.Protocol;
 
 public class test {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws NoSuchMethodException {
         //判断  @Activate注解是否在User类中使用
         System.out.println(User.class.isAnnotationPresent(Activate.class));
+
+        //获得参数类型为Protocol的构造方法 一个参数的情况 ，多个参数则列多个
+        User.class.getConstructor(Protocol.class);
 
 
 
