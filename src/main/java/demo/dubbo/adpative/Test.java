9c6435c9b50b8b7c302f8db4f3f497f147e2bd30
张东彬(DJ009828) <dj009828zhangdongbin@djbx.com>
2020-11-04 10:40:28 +0800

    源码分析一 服务注册 程序入口 service bean

---
+++ src/main/java/demo/dubbo/adpative/Test.java
@@ -0,0 +1,33 @@
+package demo.dubbo.adpative;
+
+import com.alibaba.dubbo.common.extension.ExtensionFactory;
+import com.alibaba.dubbo.common.extension.ExtensionLoader;
+import com.alibaba.dubbo.rpc.Protocol;
+
+import java.io.IOException;
+
+/**
+ * Created by DJ009828 on 2020/10/31 18:56
+ */
+public class Test {
+    public static void main(String[] args) throws IOException {
+
+
+//        Protocol protocol = ExtensionLoader.getExtensionLoader(Protocol.class).getExtension("myProtocol");
+//        System.out.println(protocol.getDefaultPort());
+
+        //动态创建一个适配器
+        Protocol protocol1 = ExtensionLoader.
+                getExtensionLoader(Protocol.class).
+                getAdaptiveExtension();
+
+        System.out.println(protocol1.getDefaultPort());
+
+
+//        Protocol protocol3 = ExtensionLoader.getExtensionLoader(Protocol.class).getDefaultExtension();
+//        System.out.println(    protocol3.getDefaultPort());
+
+
+    }
+
+}
