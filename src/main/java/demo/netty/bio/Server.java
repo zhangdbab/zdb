a9b9a272319f3f8001ee430a6f9864e7a83d9d2e
张东彬(DJ009828) <dj009828zhangdongbin@djbx.com>
2020-11-06 13:58:16 +0800

    netty

---
+++ src/main/java/demo/netty/bio/Server.java
@@ -0,0 +1,20 @@
+package demo.netty.bio;
+
+import java.net.ServerSocket;
+
+/**
+ * Created by DJ009828 on 2020/10/31 11:56
+ */
+public class Server {
+
+
+    private   static int port = 8888;
+
+     private static ServerSocket serverSocket;
+     public  static  void start(){
+         
+     }
+
+
+
+}
