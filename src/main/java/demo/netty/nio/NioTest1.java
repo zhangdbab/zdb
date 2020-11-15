e0acb3db571abd64951e3f27c61150f9e5a34938
张东彬(DJ009828) <dj009828zhangdongbin@djbx.com>
2020-11-15 16:15:43 +0800

    netty  Buffer 四个属性

--- src/main/java/demo/netty/nio/NioTest1.java
+++ src/main/java/demo/netty/nio/NioTest1.java
@@ -12,17 +12,25 @@ public class NioTest1 {
 
         //创建大小为10的缓存区
         IntBuffer buffer = IntBuffer.allocate(10);
-        for(int i=0;i<buffer.capacity();i++){
+
+//        for(int i=0;i<buffer.capacity();i++){
+        for(int i=0;i<5;i++){
             int randaomNumber = new SecureRandom().nextInt(20);
             //数据存放
             buffer.put(randaomNumber);
-
         }
+        System.out.println("before limit:"+buffer.limit()); //10
         //状态的反转 写转为读状态
         buffer.flip();
+        System.out.println("after limit:"+buffer.limit());//5
+
         //取出来
         while (buffer.hasRemaining()){
-            System.out.println(buffer.get());
+            System.out.println("position"+buffer.position());//0 1 2 3 4
+            System.out.println("limit:"+ buffer.limit());//5
+            System.out.println("capacity:"+ buffer.capacity());//10
+
+            System.out.println("value:"+ buffer.get());
         }
 
     }
