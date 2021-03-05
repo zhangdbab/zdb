3d3c8614495e855e06cac5abee7fe6c1cab97c6e
张东彬(DJ009828) <dj009828zhangdongbin@djbx.com>
2021-03-05 14:42:25 +0800

    test

--- src/main/java/demo/netty/secondexample/MyServerHander.java
+++ src/main/java/demo/netty/secondexample/MyServerHander.java
@@ -21,6 +21,7 @@ public class MyServerHander extends SimpleChannelInboundHandler<String> {
     //读取客户端发送到数据 再返回客户端数据
     protected void channelRead0(ChannelHandlerContext ctx, String msg) throws Exception {
         System.out.println("客户端远程地址："+ctx.channel().remoteAddress());
+
         System.out.println("接收客户端消息："+msg);
         ctx.channel().writeAndFlush("from server"+ UUID.randomUUID());
 
