bbfb30b86ba769f601b063ac23f6fd1623bf3c57
张东彬(DJ009828) <dj009828zhangdongbin@djbx.com>
2021-03-12 15:06:20 +0800

    mq 备用交换机

--- rabbitprovider/src/main/java/com/rabbitprovider/rabbitprovider/config/RabbitmqConfig.java
+++ rabbitprovider/src/main/java/com/rabbitprovider/rabbitprovider/config/RabbitmqConfig.java
@@ -15,6 +15,9 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
+import java.util.HashMap;
+import java.util.Map;
+
 
 @Configuration
 @ComponentScan("com")
@@ -41,11 +44,12 @@ public class RabbitmqConfig {
 
         return tomcatServletWebServerFactory;
     }
-    //发送确认，消息达到交换机后调用
+    //发送确认，
     @Bean
     public RabbitTemplate rabbitTemplate(ConnectionFactory connectionFactory){
         RabbitTemplate rabbitTemplate = new RabbitTemplate(connectionFactory);
          rabbitTemplate.setConfirmCallback(new RabbitTemplate.ConfirmCallback() {
+//             消息达到交换机后调用
             @Override
             public void confirm(CorrelationData correlationData, boolean ack, String cause) {
                 System.out.println("=====发送确认=====");
@@ -92,7 +96,12 @@ public class RabbitmqConfig {
 
     @Bean
     public DirectExchange defaultExchange() {
-        return  new DirectExchange("my_rout_exchange",true,false,null);
+//        return  new DirectExchange("my_rout_exchange",true,false,null);
+
+        Map<String, Object> map = new HashMap<>();
+        map.put("alternate-exchange","my_rout_exchange"); //备用交换机，已经存在的交换机
+        return  new DirectExchange("directExchangeTest1",false,false,map);//声明的新的交换机
+
     }
 
 
